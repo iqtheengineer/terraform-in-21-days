6090bfffa6fd1e4ad925c2a3f698b082b62d2bdc
iqtheengineer <82541131+iqtheengineer@users.noreply.github.com>
2023-05-07 14:03:29 -0400

    Task2 (#2)
    * Add first instance
    
    * adding gitignore file
    
    * Adding trailing newlines
    
    ---------
    
    Co-authored-by: Iman Crooks <iman.crooks@liveoak.bank>

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-02396cdd13e9a1257"
+  instance_type = "t3.nano"
+
+  tags = {
+    Name  = "ExampleAppServerInstance"
+    Owner = "Iman"
+  }
+}
